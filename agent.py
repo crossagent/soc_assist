fb6ef78a85fb93ad3e4bd94995b9116182a5e264
zhouxy <150273112@qq.com>
2024-09-02 20:02:38 +0800

    修改为默认openai的

--- agent.py
+++ agent.py
@@ -1,8 +1,10 @@
 from langchain_anthropic import ChatAnthropic
+from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import create_react_agent
 
-model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+#model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+model = ChatOpenAI(model="gpt-4o-mini")
 
 tools = [TavilySearchResults(max_results=2)]
 
